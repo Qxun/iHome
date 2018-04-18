1202bf14650b23e296469acdd97ef48fdab5c108
Qxun <1023552090@qq.com>
2018-04-18 13:49:26 +0800

    后端生成验证码并保存redis

--- iHome/api_1_0/verify.py
+++ iHome/api_1_0/verify.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-from flask import request
+from flask import request, abort, jsonify, make_response
+
+from iHome import redis_store, constants
+from iHome.response_code import RET
 from . import api
 from iHome.utils.captcha.captcha import captcha
 
@@ -15,8 +18,18 @@ def get_image_code():
     :return: 
     """
     # 1.获取uuid
+    uuid = request.args.get('cur_id')
+    if not uuid:
+        abort(403)
     # 2.生成图片验证码
     name, text, data = captcha.generate_captcha()
     # 3.在redis中存储图片验证码
+    try:
+        redis_store.set('imagecode:%s' %uuid, text, constants.IMAGE_CODE_REDIS_EXPIRES)
+    except Exception as e:
+        print e
+        return jsonify(errno=RET.DBERR, errmsg='保存图片验证码失败')
     # 4.返回图片验证码
-    return data
+    response = make_response(data)
+    response.headers['Content-Type'] = 'image/jpg'
+    return response
